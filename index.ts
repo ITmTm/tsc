463b6ded52ffd503cb08f027ca6553b9db085907
mtm <Stafr95@mail.ru>
2025-08-13 21:27:14 +0500

    type annotation for function

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
-const isBirthday: boolean = true;
-const age: number = 40;
-const userName: string = 'John';
+const isBirthdayData: boolean = true;
+const ageData: number = 40;
+const userNameData: string = 'John';
 
-if (isBirthday) {
-  console.log(`Congrats ${userName}, age: ${age + 1}`);
+// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+//   if (isBirthday) {
+//     return (`Congrats ${userName}, age: ${age + 1}`);
+//   } else {
+//     return 'Error!';
+//   }
+// }
+
+const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
+  if (isBirthday) {
+    return (`Congrats ${userName}, age: ${age + 1}`);
+  } else {
+    return 'Error!';
+  }
 }
+
+logBrtMsg(isBirthdayData, userNameData, ageData);
