5ae9f696b764e7544edce7b241fc52cc1ede5084
mtm <Stafr95@mail.ru>
2025-08-14 12:08:32 +0500

    type any

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-const isBirthdayData: boolean = true;
-const ageData: number = 40;
-const userNameData: string = 'John';
+// const isBirthdayData: boolean = true;
+// const ageData: number = 40;
+// const userNameData: string = 'John';
 
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-//   if (isBirthday) {
-//     return (`Congrats ${userName}, age: ${age + 1}`);
-//   } else {
-//     return 'Error!';
-//   }
-// }
+let salary: number;
+salary = 5000;
+
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "John"}';
+
+const userObj: {
+  isBirthdayData: boolean;
+  ageData: number;
+  userNameData: string;
+} = JSON.parse(userData);
+
+console.log(userObj.smt());
 
 const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
   if (isBirthday) {
